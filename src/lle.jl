60032eafa9346c1f1be7967ab7c47638177e12cd
Art Wild <wildart@gmail.com>
2014-05-17 18:40:26 -0400

    Fixed LLE

--- src/lle.jl
+++ src/lle.jl
@@ -5,12 +5,25 @@ function lle(X::Matrix; d::Int=2, k::Int=12)
 
     # Construct NN graph
     println("Building neighborhood graph...")
-    D, E = find_nn(X, k)    
+    D, E = find_nn(X, k)
 
     # Select largest connected component
     cc = components(E)
-    c = cc[indmax(map(size, cc))]
-    X = X[:,c]
+    c = cc[indmax(map(size, cc))]    
+    if length(cc) == 1
+        c = cc[1]             
+    else
+        c = cc[indmax(map(size, cc))]
+        # renumber edges    
+        R = Dict(c, 1:length(c))
+        Ec = zeros(Int,k,length(c))
+        for i = 1 : length(c)
+            Ec[:,i] = map(i->get(R,i,0), E[:,c[i]])
+        end
+        E = Ec
+        X = X[:,c]
+    end
+
 
     if k > d 
         println("   [K>D: regularization will be used]")
@@ -26,14 +39,13 @@ function lle(X::Matrix; d::Int=2, k::Int=12)
         Z = X[:, E[:,i]] .- X[:,i]
         C = Z'*Z
         C = C + eye(k, k) * tol * trace(C)
-        wi = vec(C\ones(k, 1))
-        wi /= sum(wi)
-        W[:, i] = wi
+        wi = vec(C\ones(k, 1))        
+        W[:, i] = wi./sum(wi)
     end
     
     # Compute embedding
     println("Compute embedding (solve eigenproblem)...")
-    M = spzeros(n,n)
+    M = eye(n,n) # speye(n,n)
     for i = 1 : n
         w = W[:, i]
         jj = E[:, i]
@@ -42,10 +54,10 @@ function lle(X::Matrix; d::Int=2, k::Int=12)
         M[jj,jj] = M[jj,jj] + w*w'
     end
 
-    λ, U = eigs(M, nev=d+1)
-    idx = sortperm(λ)
-    λ = λ[idx[2:(d+1)]]
-    Y = U[:,idx[2:(d+1)]]'*sqrt(n)
+    #λ, U = eigs(M, nev=d+1) # only for sparse
+    λ, U = eig(M)    
+    λ = λ[2:(d+1)] # get bottom (smallest) eigenvalues (discard first)
+    Y = U[:,2:(d+1)]'*sqrt(n)
 
     return LLE(d, k, c, λ, Y)
 end
